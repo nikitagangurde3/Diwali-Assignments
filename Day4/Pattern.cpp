653193b97413dfa3e81b2acf6d3d2dac6b51588c
nikitagangurde3 <nikitawagh696@gmail.com>
2025-10-22 17:58:22 +0530

    Question3

---
+++ Day4/Pattern.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int r;
+    cout << "Enter number of rows: ";
+    cin >> r;
+
+    for (int i = 1; i <= r; i++) {
+       
+        for (int space = i; space < r; space++) {
+            cout << " ";
+        }
+
+        
+        for (int j = 1; j <= i; j++) {
+            cout << j;
+        }
+
+        
+        for (int j = i - 1; j >= 1; j--) {
+            cout << j;
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
